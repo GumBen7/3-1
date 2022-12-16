658e83a939716fe0423739e4725517fe8aabd103
Егор Сивцев <gumben7@gmail.com>
2022-12-16 11:41:02 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ThreeOne {
+    class Program {
+        static void Main(string[] args) {
+            int b = System.Int32.Parse(System.Console.ReadLine());
+            int number = System.Int32.Parse(System.Console.ReadLine());
+            if (number == 0) {
+                System.Console.WriteLine(0);
+            }
+            System.Text.StringBuilder SB = new System.Text.StringBuilder();
+            char C;
+            while (number > 1) {            
+                if (number % b > 9) {
+                    C = (char)(number % b + 'a' - 10);
+                } 
+                else {
+                    C = (char)(number % b + '0');
+                }
+                SB.Append(C);
+                number /= b;
+            } 
+            if (number > 0) {
+                SB.Append(number);
+            }
+            char[] a = SB.ToString().ToCharArray();
+            System.Array.Reverse(a);
+            System.Console.WriteLine(a);
+        }
+    }
+}
